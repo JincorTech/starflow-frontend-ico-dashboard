56eeb23e2278f04a9d19f0b334e4feb41be77225
ValeriaVG <valeria.viana.gusmao@gmail.com>
2018-03-26 13:38:47 +0200

    Policy & terms updates

---
+++ src/containers/dashboard/TermsPopup/PrivacyPolicy.js
@@ -0,0 +1,403 @@
+import React from 'react';
+import s from './terms.css';
+
+const PrivacyPolicy = () => (
+  <div>
+    <h2>Starflow Token Sale Privacy Policy</h2>
+
+    <p>Last Updated: 23 March 2018</p>
+
+    <p>
+      Please read this Privacy Policy carefully. By registering your interest to
+      participate in the sale of STAR tokens (the “<strong>Token Sale</strong>”)
+      through any channel or method, by accessing or using the
+      https://ico.starflow.com/ website (or any successor website) (the “<strong
+      >
+        Token Sale Site
+      </strong>”), by communicating with Starflow Technology Pte. Ltd. and its
+      worldwide affiliated group companies (collectively, “<strong>
+        Starflow
+      </strong>”, “<strong>we</strong>”, “<strong>us</strong>” or “<strong>
+        our
+      </strong>”) regarding the Token Sale and/or by accepting the STAR Token
+      Sale Terms and Conditions (the “<strong>Terms</strong>”) (collectively, “<strong
+      >
+        Token Sale Activities
+      </strong>”), you consent to the collection, use, sharing and other
+      processing of your data by Starflow under this Privacy Policy. If you do
+      not agree to this Privacy Policy, do not participate in any Token Sale
+      Activity.
+    </p>
+    <h3>1. Scope of this Privacy Policy</h3>
+
+    <p>1.1 This Privacy Policy explains:</p>
+    <ul>
+      <li>What information we collect.</li>
+
+      <li>How we use, share and otherwise process personal data.</li>
+
+      <li>How we store personal data.</li>
+
+      <li>Your rights and choices in respect of your personal data.</li>
+
+      <li>
+        How you can contact us if you have questions about this Privacy Policy.
+      </li>
+    </ul>
+    <p>
+      1.2 This Privacy Policy applies when you participate in any Token Sale
+      Activity.
+    </p>
+
+    <p>
+      1.3 This Privacy Policy does not apply to the access or use of the
+      Starflow platform or to websites, applications or other services operated
+      by third parties. We encourage you to review the privacy policies posted
+      on those websites, applications and other services.
+    </p>
+
+    <p>
+      1.4 We may engage certain third-party vendors to assist us with certain
+      Token Sale Activities. This Privacy Policy applies to your personal data
+      collected by those vendors in connection with Token Sale Activities.
+      However, this Privacy Policy does not apply to personal data collected by
+      those vendors for their own purposes that is not also shared with us. For
+      example, if you submit personal data to create an account to access a
+      website operated by the vendor, that personal data will be subject to the
+      privacy policy of the vendor and not this Privacy Policy.
+    </p>
+
+    <p>
+      1.5 Ethereum is a publicly accessible, decentralised blockchain platform.
+      STAR tokens will be standard Ethereum tokens that can be viewed by anyone
+      who knows how to inspect the Ethereum blockchain. Starflow does not
+      control access to information on the Ethereum blockchain that may relate
+      to your holdings of, transactions in and other activities related to STAR
+      tokens (collectively, “<strong>Personal Blockchain Information</strong>”).
+      It may be possible, through an analysis of data about you that is publicly
+      available or that is otherwise available to persons doing the analysis, to
+      know your Personal Blockchain Information. If you are unwilling to accept
+      the risk of your Personal Blockchain Information possibly being known by
+      others, you must not participate in any Token Sale Activity.
+    </p>
+
+    <h3>2. Information We Collect</h3>
+
+    <p>We collect information from you in the following ways:</p>
+    <ul>
+      <li>
+        <em>Information provided by you</em>: The types of information we
+        collect from you include the following information you provide when you
+        participate in any Token Sale Activity:
+        <ul>
+          <li>full name;</li>
+
+          <li>identification card/passport number;</li>
+
+          <li>photograph;</li>
+
+          <li>email address;</li>
+
+          <li>telephone number;</li>
+
+          <li>residential address;</li>
+
+          <li>citizenship;</li>
+
+          <li>date of birth;</li>
+
+          <li>occupation and employer;</li>
+
+          <li>employer address, phone number and date employed;</li>
+
+          <li>source of funds;</li>
+
+          <li>
+            any personal information you may include in communications with the
+            company or in social media forums operated by or on behalf of
+            Starflow;
+          </li>
+
+          <li>digital wallet information;</li>
+
+          <li>username and password; and</li>
+
+          <li>
+            information we collect to satisfy our obligations under applicable
+            laws and regulations (including those relating to anti-money
+            laundering or countering the financing of terrorism or otherwise
+            requiring us to vet the parties we transact with) (“<strong>
+              Compliance Checks
+            </strong>”).
+          </li>
+        </ul>
+        <p>
+          If you refuse to provide any of the information listed above that we
+          identify as being required to participate in a Token Sale Activity, or
+          other information that we may request for applicable law and
+          compliance checks, you will be unable to participate in that Token
+          Sale Activity.
+        </p>
+      </li>
+      <li>
+        <em>
+          Information collected automatically through your participation in any
+          Token Sale Activity
+        </em>: We automatically collect certain information regarding your
+        participation in any Token Sale Activity, including (as applicable)
+        your:
+        <ul>
+          <li>
+            device and connection information, such as unique device identifier,
+            operating system version, hardware model, browser type and version,
+            mobile platform and mobile network information;
+          </li>
+          <li>
+            log information, such as internet protocol address, uniform resource
+            locator, clickstream data, search queries and visited pages; and
+          </li>
+          <li>location information.</li>
+        </ul>
+      </li>
+      <li>
+        <em>Information collected through cookies or similar technologies:</em>{' '}
+        Cookies are small data files stored on your computer or other device to
+        help a website remember things about you. We use cookies and similar
+        technologies in several ways to improve your experience of using the
+        Token Sale Site, including for authentication and log-in purposes, to
+        remember your preferences, to enable and support security features and
+        to collect information to help us improve our services. You may refuse
+        the use of cookies by selecting the appropriate settings on your
+        browser; however please note that if you do this you may not be able to
+        use the full functionality of the Token Sale Site or participate in any
+        Token Sale Activity.
+      </li>
+    </ul>
+    <h3>3. Use of Personal Data</h3>
+
+    <p>
+      3.1 We may use your personal data for any and all of the following
+      purposes:
+    </p>
+    <ul>
+      <li>to register your interest in participating in the Token Sale;</li>
+
+      <li>
+        to process and complete any order you might place for STAR tokens,
+        including to conduct Compliance Checks;
+      </li>
+
+      <li>to collect and manage the proceeds of the Token Sale;</li>
+
+      <li>
+        to contact you and send you promotional and marketing information by
+        telephone call, SMS, fax, post and email regarding (a) the Token Sale
+        and any future sale of STAR tokens and/or (b) the Starflow platform,
+        STAR tokens or other Starflow events, products and services;
+      </li>
+
+      <li>
+        to notify you about updates or changes to this Privacy Policy or the
+        Terms, or any aspect of the Token Sale Activities (including to inform
+        you about any terms and conditions applicable to the Token Sale);
+      </li>
+
+      <li>to respond to your communications to us;</li>
+
+      <li>
+        to detect, diagnose and fix problems affecting the Token Sale Site;
+      </li>
+
+      <li>to personalise your experience on the Token Sale Site;</li>
+
+      <li>
+        for internal business purposes, including research, analysis, testing
+        and auditing;
+      </li>
+
+      <li>
+        to comply with applicable legal obligations and other rules,
+        regulations, codes of practice and orders and directions of competent
+        governmental authorities that we are obligated to follow;
+      </li>
+
+      <li>
+        to prevent, detect, investigate and prosecute threats, fraud, misconduct
+        or other unlawful or malicious activity;
+      </li>
+
+      <li>to enforce our agreements and resolve disputes; and</li>
+
+      <li>
+        to protect our rights and property and the rights, property and safety
+        of others.
+      </li>
+    </ul>
+
+    <p>
+      3.2 We may use and disclose automatically collected information (including
+      aggregated information), which is not personal data, for any purpose,
+      except where we are restricted by applicable law. If we combine any
+      automatically collected information with personal data or other
+      information which may be used to identify an individual, the combined
+      information will be treated by us as personal data in accordance with this
+      Privacy Policy.
+    </p>
+    <h3>4. Sharing and Cross-Border Transfer of Personal Data</h3>
+
+    <p>4.1 We share your personal data in the following ways:</p>
+    <ul>
+      <li>
+        <em>
+          Our business partners, service providers, professional advisers,
+          vendors and agents
+        </em>: Your personal data may be accessible to our business partners,
+        service providers, professional advisers, vendors and agents that
+        provide services to us (including those that provide compliance check,
+        Token Sale-related, data storage, maintenance, legal, audit, insurance
+        and marketing services) in connection with the conduct of the Token
+        Sale.
+      </li>
+
+      <li>
+        <em>
+          Our potential investors, purchasers and commercial counterparties
+        </em>: We may share your personal data with any person who wishes to
+        invest in Starflow or the Starflow platform or whom we intend to enter
+        into a business combination with (such as a purchase, sale, lease,
+        merger or amalgamation, or any other acquisition, disposal or
+        financing).
+      </li>
+
+      <li>
+        <em>Governmental and regulatory authorities</em>: We may share your
+        personal data with competent governmental and regulatory agencies in
+        Singapore, Sweden or other countries, including investigating officers
+        and judicial authorities in the relevant jurisdictions.
+      </li>
+
+      <li>
+        Any information which we share with third parties under this paragraph
+        4.1 shall be used for the purposes described in this Privacy Policy,
+        including as provided in paragraph 3 (Use of Personal Data) above.
+      </li>
+    </ul>
+    <p>
+      4.2 We may also share your personal data with other third parties if you
+      have separately consented to such disclosure. In such case, any
+      information which we share with such third parties shall be used for the
+      purposes described when we obtained your consent.
+    </p>
+
+    <p>
+      4.3 We may share your personal data with any other person where such
+      disclosure is required or permitted under applicable law or regulation,
+      and we may be prevented from obtaining your consent for, or informing you
+      of, such disclosure. Any information which we share in this circumstance
+      shall be used for the purposes required or permitted under applicable law
+      or regulation.
+    </p>
+
+    <p>
+      4.4 Your personal data may be transferred to other countries (including
+      Sweden and Germany) for processing, and by participating in any Token Sale
+      Activity you consent to the transfer, use and processing of your personal
+      data to countries outside of Singapore which may have different personal
+      data protection rules than in Singapore. However, the level of protection
+      that will be applied to the transferred personal information will be at
+      least comparable to the protection provided under this Privacy Policy and
+      the Singapore Personal Data Protection Act 2012 and its regulations.
+    </p>
+    <h3>5. Storing Personal Data</h3>
+
+    <p>
+      5.1 All information collected in connection with any Token Sale Activity
+      (including information generated by cookies about your use of the Token
+      Sale Site) will be stored electronically on the server of our service
+      provider in Germany with appropriate safeguards and security measures.
+      However, there is no guarantee that information stored by us or on our
+      behalf may not be accessed, collected, used, disclosed, copied, modified
+      or deleted by unauthorised parties by breach of any technical or
+      operational safeguards in place. We will not be keeping physical files of
+      any personal data.
+    </p>
+    <p>
+      5.2 No data transmitted via the Internet can be guaranteed to be wholly
+      secure during transmission. As such, we cannot warrant the security of any
+      information you transmit to us or which we transmit to you before its
+      receipt and any transmission is at your own risk.
+    </p>
+
+    <p>
+      5.3 You are at all times responsible for your own Internet privacy
+      settings. We are not responsible, and accept no liability, for the
+      functionality, privacy or security measures of any other organisation or
+      third-party websites, applications or services.
+    </p>
+    <h3>6. Rights Concerning Personal Data</h3>
+
+    <p>
+      6.1 You can manage available communications preferences when you access
+      the Token Sale Site by updating your account preferences, or, where
+      applicable, by using the “<strong>opt-out</strong>” or unsubscribe
+      mechanism or other means provided within the communications that you
+      receive from us.
+    </p>
+    <p>
+      6.2 Subject to exceptions under applicable law and regulations, you may
+      request access to your personal data that we collected in connection with
+      any Token Sale Activity and information about how we have used and
+      disclosed your personal data within a year before your request. Subject to
+      exceptions under applicable law and regulations, you may also request to
+      review and correct any of that personal data. Additional rights may also
+      apply under applicable laws. We reserve the right to charge a nominal fee
+      in respect of our costs in processing your request for access to personal
+      data.
+    </p>
+    <p>
+      6.3 You may decide at any time to withdraw your consent to our use of your
+      personal data in any manner or for a particular purpose. However, please
+      note that any withdrawal of your consent may have consequences, including
+      your inability to participate in any Token Sale Activity. Please also note
+      that notwithstanding the withdrawal of your consent, we reserve the right
+      to retain your personal data for as long as is necessary for our legal or
+      business purposes.
+    </p>
+    <p>
+      6.4 Any request for access to or correction of personal data or any notice
+      for withdrawal of consent to use personal data should be addressed to us
+      according to paragraph 8 (Contacting Us) below. To protect your privacy
+      and security, we will take reasonable steps to verify your identity before
+      processing any access or correction request or withdrawal notice under
+      paragraphs 6.2 and 6.3 above.
+    </p>
+    <h3>7. Updates to this Privacy Policy</h3>
+
+    <p>
+      We reserve the right to update, modify or otherwise make changes to this
+      Privacy Policy at any time. Any changes to this Privacy Policy will be
+      posted to the Token Sale Site. A notification may also be sent to your
+      email address. Any changes to this Privacy Policy will take effect and be
+      binding on you from the time of posting the changes to the Token Sale
+      Site. If you continue to participate in any Token Sale Activity following
+      any changes to this Privacy Policy, you will be deemed to have accepted
+      the changes and the updated version of this Privacy Policy. If you do not
+      agree with the changes, please stop participating in any Token Sale
+      Activity. Please ensure that you refer to this Privacy Policy on a regular
+      basis to check for any changes.
+    </p>
+
+    <h3>8. Contacting Us</h3>
+
+    <p>
+      If you have any comments or questions about this Privacy Policy or our
+      processing of your information, please contact us at:
+    </p>
+    <p>Starflow Technology Pte. Ltd.</p>
+
+    <p>Email: legal@starflow.com</p>
+    <div className={s.remainder} />
+  </div>
+);
+
+export default PrivacyPolicy;
